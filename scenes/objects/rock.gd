eba9328de396c403014ce356a14d2616e508ba38
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-09-21 20:28:47 +0630

    adding rock explosion effect

--- scenes/objects/rock.gd
+++ scenes/objects/rock.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+signal exploded
+
 var screensize = Vector2.ZERO
 var size
 var radius
@@ -16,9 +18,38 @@ func start(_position, _velocity, _size):
 	size = _size
 	mass = 1.5 * size
 	$Sprite2D.scale = Vector2.ONE * scale_factor * size
+	$Explosion.scale = Vector2.ONE * 0.75 * size;
 	radius = int($Sprite2D.texture.get_size().x / 2 * $Sprite2D.scale.x)
 	var shape = CircleShape2D.new()
 	shape.radius = radius
 	$CollisionShape2D.shape = shape
 	linear_velocity = _velocity
 	angular_velocity = randf_range(PI, -PI)
+	
+func explode():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
